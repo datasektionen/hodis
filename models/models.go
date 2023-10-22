5abe3ffeaba582b30e0860b62573e7fe3df2223f
Mathias Magnusson <mathias@magnusson.space>
2023-10-22 18:22:13 +0200

    Make solution for permanent members use a bool

--- models/models.go
+++ models/models.go
@@ -15,24 +15,13 @@ type User struct {
 	GivenName   string `form:"givenName"   json:"givenName"`
 	DisplayName string `form:"displayName" json:"displayName"`
 
-	// The date they are currently known to be chapter members until. `MemberToForever` is a special
-	// case which means that the person should never stop being considered a chapter member. This
-	// should overrule dates given by THS. TODO: change this before year 9998.
-	MemberTo *time.Time `form:"memberTo" json:"memberTo"`
-	Year     int        `form:"year"     json:"year"`
-	Tag      string     `form:"tag"      json:"tag"`
+	// Should not be considered when `PermanentMember` is true
+	MemberTo        *time.Time `form:"memberTo"        json:"memberTo"`
+	PermanentMember bool       `form:"permanentMember" json:"permanentMember" gorm:"default:false"`
+	Year            int        `form:"year"            json:"year"`
+	Tag             string     `form:"tag"             json:"tag"`
 
 	Refs uint `json:"-"`
-}
-
-var MemberToForever time.Time
-
-func init() {
-	var err error
-	MemberToForever, err = time.Parse(time.DateOnly, "9999-12-24")
-	if err != nil {
-		panic(err)
-	}
 }
 
 type Token struct {
